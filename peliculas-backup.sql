5003bbeb3a7937d03d5922db8c4f2fe1b60b118b
javaee <javaee@javaee-VirtualBox>
2020-06-09 14:04:03 +0200

    diagrama corregido, update del backup de la bbdd

--- peliculas-backup.sql
+++ peliculas-backup.sql
@@ -32,9 +32,13 @@ DROP TABLE IF EXISTS `peliculas`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `peliculas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) COLLATE latin1_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  `nombre` varchar(100) COLLATE latin1_spanish_ci NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `anio` int(11) NOT NULL,
+  `caratula` varchar(255) COLLATE latin1_spanish_ci NOT NULL DEFAULT 'https://picsum.photos/50/75',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `peliculas_UN` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +47,7 @@ CREATE TABLE `peliculas` (
 
 LOCK TABLES `peliculas` WRITE;
 /*!40000 ALTER TABLE `peliculas` DISABLE KEYS */;
-INSERT INTO `peliculas` VALUES (1,'la milla verde'),(2,'waterworld'),(3,'scary movie'),(4,'la jungla de cristal'),(5,'el señor de los anillos'),(6,'el caballero oscuro'),(7,'black hawk derribado'),(8,'serenity'),(9,'matrix'),(10,'iron man 3'),(11,'vengadores infinity war');
+INSERT INTO `peliculas` VALUES (1,'la milla verde',125,1995,'https://picsum.photos/50/75'),(2,'waterworld',98,1997,'https://picsum.photos/50/75'),(3,'scary movie',86,2001,'https://picsum.photos/50/75'),(4,'la jungla de cristal',96,1996,'https://picsum.photos/50/75'),(5,'el señor de los anillos',176,2008,'https://picsum.photos/50/75'),(6,'el caballero oscuro',153,2010,'https://picsum.photos/50/75'),(7,'black hawk derribado',118,1998,'https://picsum.photos/50/75'),(8,'serenity',100,2003,'https://picsum.photos/50/75'),(9,'matrix',110,2006,'https://picsum.photos/50/75'),(10,'iron man 3',123,2015,'https://picsum.photos/50/75'),(11,'vengadores infinity war',159,2019,'https://picsum.photos/50/75'),(12,'la guerra de las galaxias',127,1984,'https://picsum.photos/50/75'),(17,'jumanji',103,1996,'https://picsum.photos/50/75'),(19,'las dos torres',183,2007,'https://picsum.photos/50/75');
 /*!40000 ALTER TABLE `peliculas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-08 12:53:33
+-- Dump completed on 2020-06-09 14:02:53
